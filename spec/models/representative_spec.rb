f1ef582e2470a06ab4732610810e80d096c85d14
Chithra Anand <chithra.rajan@berkeley.edu>
2022-11-21 23:07:49 +0000

    fixed test to have better variable naming

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -8,10 +8,10 @@ require 'json'
 RSpec.describe Representative, type: :model do
   describe 'civic_api_to_representative_params method' do
     it 'does not create representative records that already exist' do
-      result = eval(File.read('spec/models/representative_stub.txt'))
-      result = OpenStruct.new(JSON.parse(result.to_json, object_class: OpenStruct))
-      @representatives = described_class.civic_api_to_representative_params(result)
-      @representatives2 = described_class.civic_api_to_representative_params(result)
+      sample_api_res = eval(File.read('spec/models/representative_stub.txt'))
+      sample_api_res_serialized_obj = OpenStruct.new(JSON.parse(sample_api_res.to_json, object_class: OpenStruct))
+      @representatives = described_class.civic_api_to_representative_params(sample_api_res_serialized_obj)
+      @representatives2 = described_class.civic_api_to_representative_params(sample_api_res_serialized_obj)
       count = 0
       @representatives2.each do |rep|
         count += 1 if rep.name == 'Kamala D. Harris'
