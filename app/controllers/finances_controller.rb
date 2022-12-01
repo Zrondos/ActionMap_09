0b7c8ca98b24599e3354baffcc2ac7c3765e93de
Rajendra Goudar <rajendragoudar5@gmail.com>
2022-11-30 22:14:05 -0800

    sad pathss for campaign finance page are working

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+ 
 class FinancesController < ApplicationController
   def index
     # @finances = Finance.all
@@ -14,21 +14,32 @@ class FinancesController < ApplicationController
                       'Refund Total']
     @cycle_list = %w[2010 2012 2014 2016 2018 2020]
   end
-
+  
   def search
     cycle = params[:cycle][0]
     category = params[:category][0]
     category = Finance.get_category(category)
-    uri = 'https://api.propublica.org'
-    url = URI.parse(uri)
-    port = '443'
-    http = Net::HTTP.new(url.host, port)
-    http.use_ssl = true
-    uri = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
-    result, _data = http.get(uri, 'x-api-key' => 'BDfqKXZfoJakFhcim5KU0NtCOUCwFXrxkaGmrL4O')
-    Rails.logger.debug result.body
-    result = JSON.parse(result.body, object_class: OpenStruct)
-    @finances = Finance.campaign_finance_api_to_finance_params(result)
-    render 'finances/search'
+    if !(category.nil?) && !(cycle.empty?)
+      @cyc = cycle
+      @cat = category
+      uri = 'https://api.propublica.org'
+      url = URI.parse(uri)
+      port = '443'
+      http = Net::HTTP.new(url.host, port)
+      http.use_ssl = true
+      uri = "https://api.propublica.org/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json"
+      result, _data = http.get(uri, 'x-api-key' => 'BDfqKXZfoJakFhcim5KU0NtCOUCwFXrxkaGmrL4O')
+      Rails.logger.debug result.body
+      result = JSON.parse(result.body, object_class: OpenStruct)
+      @finances = Finance.campaign_finance_api_to_finance_params(result)
+      render 'finances/search'
+    elsif category.nil? && cycle.empty?
+      redirect_to finances_path, alert: 'Please select a cycle and category'
+    elsif category.nil?
+      redirect_to finances_path, alert: 'Please select a category'
+    elsif cycle.empty?
+      redirect_to finances_path, alert: 'Please select a cycle'
+    end
   end
-end
+ end
+ 
