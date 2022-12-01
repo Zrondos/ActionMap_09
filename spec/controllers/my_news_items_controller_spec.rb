627913a9af30313c369032652ff1214fc24f39f8
Baptiste Marchand <baptiste.marchand4@protonmail.com>
2022-11-30 18:26:34 -0800

    Fix pronto repeated code segment

--- spec/controllers/my_news_items_controller_spec.rb
+++ spec/controllers/my_news_items_controller_spec.rb
@@ -4,14 +4,16 @@ require 'rails_helper'
 
 RSpec.describe MyNewsItemsController, type: :controller do
   describe 'set variables' do
-    it 'sets the issues list' do
+    before do
       @my_news_items_controller = described_class.new
+    end
+
+    it 'sets the issues list' do
       @my_news_items_controller.send(:set_issues_list)
       expect(@my_news_items_controller.instance_variable_get(:@issues_list)).not_to be_nil
     end
 
     it 'sets the representatives list' do
-      @my_news_items_controller = described_class.new
       @my_news_items_controller.send(:set_representatives_list)
       expect(@my_news_items_controller.instance_variable_get(:@representatives_list)).not_to be_nil
     end
